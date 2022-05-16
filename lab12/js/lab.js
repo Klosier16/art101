df783e12d8e296e3533c59a92ae85255bc3d88ff
Kenneth Losier <klosier@ucsc.edu>
2022-05-16 08:31:51 -0700

    Created Lab12 in ART101 Folder

---
+++ lab12/js/lab.js
@@ -0,0 +1,13 @@
+/*
+ * Author: Shayan Bathaee & Kenny Losier
+ * Created: 16 May, 2022
+ * Description: This file contains the JavaScript code for our Lab 10 Assignment
+ * Files:
+ *  lab.js - The file you are in right now. Contains the JavaScript code for this lab
+ *  index.html - Contains description of the lab, including challenges, problems
+ *               and results. This file also contains the linked JavaScript file
+ *           and CSS file.
+ *  lab.css - This file contains the stylesheet for index.html
+ *
+ * Licence: Public Domain
+ */
