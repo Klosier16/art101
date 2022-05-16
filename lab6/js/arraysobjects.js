ccf5c14ffbc208f95d060f1188d4e1721a92b7be
Kenneth Losier <klosier@ucsc.edu>
2022-05-16 08:32:28 -0700

    Updated Lab6

--- lab6/js/arraysobjects.js
+++ lab6/js/arraysobjects.js
@@ -3,11 +3,11 @@
  * Created: 25 April, 2022
  * Description: This file contains the JavaScript code for our Lab 6 Assignment
  * Files:
- *  lab.js - The file you are in right now. Contains the JavaScript code for this
+ *  arraysobjects.js - The file you are in right now. Contains the JavaScript code for this
  *           lab.
- *  index.html - Contains description of the lab, including challenges, problems
+ *  arraysobjects.html - Contains description of the lab, including challenges, problems
  *               and results. This file also contains the linked JavaScript file
- *  lab.css - This file contains the stylesheet for index.html
+ *  arraysobjects.css - This file contains the stylesheet for index.html
  *
  * Licence: Public Domain
  */
