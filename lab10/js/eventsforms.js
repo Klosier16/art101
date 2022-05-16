54a333a08b21b37b8a1945fdbc911dcdfe72e1ee
Kenneth Losier <klosier@ucsc.edu>
2022-05-16 08:32:07 -0700

    Updated Lab10

--- lab10/js/eventsforms.js
+++ lab10/js/eventsforms.js
@@ -3,11 +3,11 @@
  * Created: 09 May, 2022
  * Description: This file contains the JavaScript code for our Lab 10 Assignment
  * Files:
- *  lab.js - The file you are in right now. Contains the JavaScript code for this
+ *  eventsforms.js - The file you are in right now. Contains the JavaScript code for this
  *           lab.
- *  index.html - Contains description of the lab, including challenges, problems
+ *  eventsforms.html - Contains description of the lab, including challenges, problems
  *               and results. This file also contains the linked JavaScript file
- *  lab.css - This file contains the stylesheet for index.html
+ *  eventsforms.css - This file contains the stylesheet for index.html
  *
  * Licence: Public Domain
  */
