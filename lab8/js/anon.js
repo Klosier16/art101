e23d793da02b119634bbf49797fede0ee194bba1
Kenneth Losier <klosier@ucsc.edu>
2022-05-16 08:32:45 -0700

    Updated Lab8

--- lab8/js/anon.js
+++ lab8/js/anon.js
@@ -3,11 +3,11 @@
  * Created: 04 May, 2022
  * Description: This file contains the JavaScript code for our Lab 9 Assignment
  * Files:
- *  lab.js - The file you are in right now. Contains the JavaScript code for this
+ *  anon.js - The file you are in right now. Contains the JavaScript code for this
  *           lab.
- *  index.html - Contains description of the lab, including challenges, problems
+ *  anon.html - Contains description of the lab, including challenges, problems
  *               and results. This file also contains the linked JavaScript file
- *  lab.css - This file contains the stylesheet for index.html
+ *  anon.css - This file contains the stylesheet for index.html
  *
  * Licence: Public Domain
  */
