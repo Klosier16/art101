121e534eda66667f01e4a31e3c71c6cc25412b69
Kenneth Losier <klosier@ucsc.edu>
2022-05-16 08:32:35 -0700

    Updated Lab7

--- lab7/js/functions.js
+++ lab7/js/functions.js
@@ -3,11 +3,11 @@
  * Created: 27 April, 2022
  * Description: This file contains the JavaScript code for our Lab 7 Assignment
  * Files:
- *  lab.js - The file you are in right now. Contains the JavaScript code for this
+ *  functions.js - The file you are in right now. Contains the JavaScript code for this
  *           lab.
- *  index.html - Contains description of the lab, including challenges, problems
+ *  functions.html - Contains description of the lab, including challenges, problems
  *               and results. This file also contains the linked JavaScript file
- *  lab.css - This file contains the stylesheet for index.html
+ *  functions.css - This file contains the stylesheet for index.html
  *
  * Licence: Public Domain
  */
