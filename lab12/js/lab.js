f132e7612e60a9793dd4e1140ba13e7a43beb2f1
Kenneth Losier <klosier@ucsc.edu>
2022-05-16 22:34:31 -0700

    Updated/Finalized Lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -11,3 +11,108 @@
  *
  * Licence: Public Domain
  */
+
+function sortingHat(name) {
+    var length = name.length;
+    var mod = length % 4;
+    if (mod == 0) {
+      return "Hufflepuff";
+    }
+    else if (mod == 1) {
+      return "Slytherin";
+    }
+    else if (mod == 2) {
+      return "Ravenclaw";
+    }
+    else if (mod == 3) {
+      return "Gryffindor";
+    }
+}
+
+$("#sortButton").click(function() {
+  var name = $("#input").val();
+  var result = sortingHat(name);
+
+  if (result == "Hufflepuff") {
+    if ($("#out").length == 0) {
+      $("#output").append("<p id='out'>The Sorting Hat has sorted you into <span id='hufflepuff'>Hufflepuff!</span></p>");
+    }
+    else {
+      $("#out").replaceWith("<p id='out'>The Sorting Hat has sorted you into <span id='hufflepuff'>Hufflepuff!</span></p>");
+    }
+    if ($("#description").length == 0) {
+      $("#output").append("<p id='description'>Hufflepuff is where you will find the most trustworthy and hardworking students. In " +
+      "fact, out of all the houses Hufflepuff has produced the least number of dark witches and wizards. The badger " +
+      "is the symbol of this house, the colours are yellow and black, the Head of House is Professor Pomona Sprout " +
+      "and the common room can be found near the kitchens in Hogwarts.</p>");
+    }
+    else {
+      $("#description").replaceWith("<p id='description'>Hufflepuff is where you will find the most trustworthy and hardworking students. In " +
+      "fact, out of all the houses Hufflepuff has produced the least number of dark witches and wizards. The badger " +
+      "is the symbol of this house, the colours are yellow and black, the Head of House is Professor Pomona Sprout " +
+      "and the common room can be found near the kitchens in Hogwarts.</p>");
+    }
+  }
+
+  if (result == "Slytherin") {
+    if ($("#out").length == 0) {
+      $("#output").append("<p id='out'>The Sorting Hat has sorted you into <span id='slytherin'>Slytherin!</span></p>");
+    }
+    else {
+      $("#out").replaceWith("<p id='out'>The Sorting Hat has sorted you into <span id='slytherin'>Slytherin!</span></p>");
+    }
+    if ($("#description").length == 0) {
+      $("#output").append("<p id='description'>Slytherin house has an unfortunate reputation. While it is true that a lot of dark " +
+      "witches and wizards were sorted into Slytherin, not all who belong to this house are bad. In fact, there " +
+      "are a lot of excellent qualities the Sorting Hat looks for in potential Slytherins and Merlin himself even " +
+      "belonged to this misunderstood house!</p>");
+    }
+    else {
+      $("#description").replaceWith("<p id='description'>Slytherin house has an unfortunate reputation. While it is true that a lot of dark " +
+      "witches and wizards were sorted into Slytherin, not all who belong to this house are bad. In fact, there " +
+      "are a lot of excellent qualities the Sorting Hat looks for in potential Slytherins and Merlin himself even " +
+      "belonged to this misunderstood house!</p>");
+    }
+  }
+
+  if (result == "Ravenclaw") {
+    if ($("#out").length == 0) {
+      $("#output").append("<p id='out'>The Sorting Hat has sorted you into <span id='ravenclaw'>Ravenclaw!</span></p>");
+    }
+    else {
+      $("#out").replaceWith("<p id='out'>The Sorting Hat has sorted you into <span id='ravenclaw'>Ravenclaw!</span></p>");
+    }
+    if ($("#description").length == 0) {
+      $("#output").append("<p id='description'>If you are looking for the brainiest students, you would find them in Ravenclaw " +
+      "(with a couple of notable exceptions like Hermione Granger). The Ravenclaw colours are blue and bronze, " +
+      "the emblem is an eagle, the Head of House is Professor Filius Flitwick and the common room sits at the " +
+      "top of Ravenclaw Tower behind an enchanted knocker.</p>");
+    }
+    else {
+      $("#description").replaceWith("<p id='description'>If you are looking for the brainiest students, you would find them in Ravenclaw " +
+      "(with a couple of notable exceptions like Hermione Granger). The Ravenclaw colours are blue and bronze, " +
+      "the emblem is an eagle, the Head of House is Professor Filius Flitwick and the common room sits at the " +
+      "top of Ravenclaw Tower behind an enchanted knocker.</p>");
+    }
+  }
+
+  if (result == "Gryffindor") {
+    if ($("#out").length == 0) {
+      $("#output").append("<p id='out'>The Sorting Hat has sorted you into <span id='gryffindor'>Gryffindor!</span></p>");
+    }
+    else {
+      $("#out").replaceWith("<p id='out'>The Sorting Hat has sorted you into <span id='gryffindor'>Gryffindor!</span></p>");
+    }
+    if ($("#description").length == 0) {
+      $("#output").append("<p id='description'>Gryffindor house is where you would find the pluckiest and most daring students " +
+      "(there is a reason the house symbol is the brave lion). The house colours are scarlet and gold, the common " +
+      "room lies up in Gryffindor Tower and the Head of House is Professor Minerva McGonagall.</p>");
+    }
+    else {
+      $("#description").replaceWith("<p id='description'>Gryffindor house is where you would find the pluckiest and most daring students " +
+      "(there is a reason the house symbol is the brave lion). The house colours are scarlet and gold, the common " +
+      "room lies up in Gryffindor Tower and the Head of House is Professor Minerva McGonagall.</p>");
+    }
+  }
+
+});
