00cc991bd5c0255028dfffd074908af97be501df
Kenneth Losier <klosier@ucsc.edu>
2022-05-25 17:58:08 -0700

    Updated Lab15 With Working AJAX

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -11,3 +11,31 @@
  *
  * Licence: Public Domain
  */
+
+$("#activate").click(function() {
+  var randomID = Math.floor(Math.random() * 2624); // generate a random comic ID up to their most recent comic
+  $.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://xkcd.com/" + randomID + "/info.0.json",
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successfully
+    success: function(data) {
+        // put the image into the HTML document
+        var img_link = data["img"];
+        if ($("#comicImg").length == 0) {
+          $("#output").append("<img src='" + img_link + "' alt='comic' width=600px id='comicImg'>");
+        }
+        else {
+          $("#comicImg").replaceWith("<img src='" + img_link + "'alt='comic' width=600px id='comicImg'>");
+        }
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+        // log error
+        console.log("Error:", textStatus, errorThrown);
+    }
+  })
+});
